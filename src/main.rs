5c61a410f1f8b06c90bbdf11050efd49a8b6e801
stephaneworkspace <s.bressani@bluewin.ch>
2020-04-13 17:22:16 +0200

    state is awesome

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,21 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+use std::sync::Mutex;
 
 struct AppState {
-    _foo: String,
-}
-/*
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    year: i32,
+    month: i32,
+    day: i32,
+    hour: i32,
+    hourf32: f64,
+    min: i32,
 }
 
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
-}
-*/
 
-#[get("/api/hello")]
-async fn index3() -> impl Responder {
+#[get("/api/svg/natal.svg")]
+async fn index3(data: web::Data<Mutex<AppState>>) -> impl Responder {
+    let data = data.lock().unwrap();
     const PATH: &str = "data.json";
     let mut s = String::new();
     let mut file_path = PathBuf::new();
@@ -35,7 +34,7 @@ async fn index3() -> impl Responder {
         .unwrap()
         .read_to_string(&mut s)
         .unwrap();
-    let data: astrology::Data = serde_json::from_str(&s).unwrap();
+    let a_data: astrology::Data = serde_json::from_str(&s).unwrap();
     let path_str: String = format!("{}/swisseph-for-astrology-crate/", env::current_dir().unwrap().as_path().display().to_string());
     println!("{}", path_str);
     let d = DataChartNatalC {
@@ -45,9 +44,9 @@ async fn index3() -> impl Responder {
         hourf32: data.hourf32,
         hour: data.hour,
         min: data.min,
-        sec: data.sec,
-        lat: data.lat,
-        lng: data.lng,
+        sec: a_data.sec,
+        lat: a_data.lat,
+        lng: a_data.lng,
     };
     let res: Vec<DataObjectSvg> = astrology::svg_draw::chart(1000.0, d, path_str.as_str());
     let mut svg_res: String = "".to_string();
@@ -90,14 +89,20 @@ async fn main() -> std::io::Result<()> {
 }
 
 fn app_config(config: &mut web::ServiceConfig) {
+    let data = web::Data::new(Mutex::new(AppState {
+                year: 2000,
+                month: 1,
+                day: 1,
+                hour: 0,
+                hourf32: 0.0,
+                min: 0
+            }));
     config.service(
         web::scope("")
-            .data(AppState {
-                _foo: "bar".to_string(),
-            })
-            .service(web::resource("/").route(web::get().to(index)))
+            .app_data(data.clone())
+            .service(web::resource("/api/").route(web::get().to(index)))
             .service(index3)
-            .service(web::resource("/natal_chart").route(web::post().to(handle_post_natal_chart))),
+            .service(web::resource("/api/natal_chart").route(web::post().to(handle_post_natal_chart))),
         );
 }
 
@@ -107,13 +112,28 @@ async fn index() -> Result<HttpResponse> {
         .body(include_str!("../static/form.html")))
 }
 
-async fn handle_post_natal_chart(params: web::Form<MyParams>) -> Result<HttpResponse> {
+async fn handle_post_natal_chart(params: web::Form<MyParams>, data: web::Data<Mutex<AppState>>) -> Result<HttpResponse> {
+    let mut data = data.lock().unwrap();
+    data.year = params.year;
+    data.month = params.month;
+    data.day = params.day;
+    data.hour = params.hour;
+    data.hourf32 = params.hourf32;
+    data.min = params.min;
+    let svg = "<img src=\"http://0.0.0.0:8088/api/svg/natal.svg\"/>";
     Ok(HttpResponse::Ok()
-        .content_type("text/plain")
-        .body(format!("Your name is {}", params.name)))
+        .content_type("text/html; charset=utf-8")
+        .body(format!("<html>Your year is {}{}<br />{}</html>", params.year, data.year, svg)))
 }
+
 
 #[derive(Serialize, Deserialize)]
 pub struct MyParams {
-    name: String,
+    year: i32,
+    month: i32,
+    day: i32,
+    hour: i32,
+    hourf32: f64,
+    min: i32,
 }
+
