e12541855f857acde3224ef3647c4a1cb8e256ec
stephaneworkspace <s.bressani@bluewin.ch>
2020-05-23 19:20:19 +0200

    preapre to transit

--- src/main.rs
+++ src/main.rs
@@ -114,6 +114,7 @@ fn app_config(config: &mut web::ServiceConfig) {
             .service(index3)
             .service(web::resource("/api/natal_chart").route(web::post().to(handle_post_natal_chart)))
             .service(web::resource("/api/svg_chart").route(web::post().to(handle_post_natal_chart_svg)))
+            .service(web::resource("/api/svg_chart_transit").route(web::post().to(handle_post_natal_chart_svg_transit)))
             .service(all_aspects),
         );
 }
@@ -141,13 +142,6 @@ async fn handle_post_natal_chart(params: web::Form<MyParams>, data: web::Data<Mu
 
 /// Svg only
 async fn handle_post_natal_chart_svg(params: web::Form<MyNatalParams>, _data: web::Data<Mutex<AppState>>) -> Result<HttpResponse> {
-    /*let mut data = data.lock().unwrap();
-    data.year = params.year;
-    data.month = params.month;
-    data.day = params.day;
-    data.hour = params.hour;
-    data.min = params.min;
-    */
     let path_str: String = format!("{}/swisseph-for-astrology-crate/", env::current_dir().unwrap().as_path().display().to_string());
     println!("{}", path_str);
     let d = DataChartNatal {
@@ -161,6 +155,198 @@ async fn handle_post_natal_chart_svg(params: web::Form<MyNatalParams>, _data: we
         lng: params.lng,
     };
     let res: Vec<DataObjectSvg> = astrology::svg_draw::chart(600.0, d, path_str.as_str(), Language::French);
+    let mut svg_res: String = "".to_string();
+    for r in res.clone() {
+        if r.object_type == DataObjectType::Chart {
+            svg_res = r.svg;
+        }
+    }
+    if svg_res != "" {
+        svg_res = svg_res.replace("</svg>", "");
+        for r in res {
+            if r.object_type != DataObjectType::Chart {
+                if r.object_type == DataObjectType::Aspect {
+                    match params.aspect {
+                        0 => {
+                        },
+                        1 => {
+                            let mut sw_res = false;
+                            // Major
+                            for a in r.aspects {
+                                if a.to_string() == "Conjunction".to_string() || a.to_string() == "Opposition".to_string() || a.to_string() == "Trine".to_string() || a.to_string() == "Square".to_string() || a.to_string() == "Sextile".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        2 => {
+                            let mut sw_res = false;
+                            // Conjunction
+                            for a in r.aspects {
+                                if a.to_string() == "Conjunction".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        3 => {
+                            let mut sw_res = false;
+                            // Opposition
+                            for a in r.aspects {
+                                if a.to_string() == "Opposition".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        4 => {
+                            let mut sw_res = false;
+                            // Trigone
+                            for a in r.aspects {
+                                if a.to_string() == "Trine".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        5 => {
+                            let mut sw_res = false;
+                            // Square
+                            for a in r.aspects {
+                                if a.to_string() == "Square".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        6 => {
+                            let mut sw_res = false;
+                            // Sextile
+                            for a in r.aspects {
+                                if a.to_string() == "Sextile".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        7 => {
+                            let mut sw_res = false;
+                            // Minor
+                            for a in r.aspects {
+                                if a.to_string() == "Inconjunction".to_string() || a.to_string() == "Sesquisquare".to_string() || a.to_string() == "Semisquare".to_string() || a.to_string() == "Semisextile".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        8 => {
+                            let mut sw_res = false;
+                            // Inconjunction
+                            for a in r.aspects {
+                                if a.to_string() == "Inconjunction".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        9 => {
+                            let mut sw_res = false;
+                            // Sesquisquare
+                            for a in r.aspects {
+                                if a.to_string() == "Sesquisquare".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        10 => {
+                            let mut sw_res = false;
+                            // Semisquare
+                            for a in r.aspects {
+                                if a.to_string() == "Semisquare".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        11 => {
+                            let mut sw_res = false;
+                            // Semisextile
+                            for a in r.aspects {
+                                if a.to_string() == "Semisextile".to_string() {
+                                    sw_res = true;
+                                }
+                            }
+                            if sw_res {
+                                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()))
+                            }
+                        },
+                        12 => svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str())),
+                        _ => svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str())),
+                    }
+                 } else {
+                    // to do better inside after for real use
+                    svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()));
+                }
+            } else {
+                // to do better inside after for real use
+                svg_res = format!("{}<image width=\"{}\" height=\"{}\" x=\"{}\" y=\"{}\" href=\"data:image/svg+xml;base64,{}\"/>", svg_res, r.size_x, r.size_y, r.pos_x, r.pos_y, encode(r.svg.as_str()));
+            }
+        }
+    } else {
+        svg_res = "<svg>".to_string();
+    }
+    svg_res = format!("{}</svg>", svg_res);
+ 
+     Ok(HttpResponse::Ok()
+        .content_type("image/svg+xml")
+        .body(svg_res))
+}
+/// Svg only transit
+async fn handle_post_natal_chart_svg_transit(params: web::Form<MyNatalParams>, _data: web::Data<Mutex<AppState>>) -> Result<HttpResponse> {
+    let path_str: String = format!("{}/swisseph-for-astrology-crate/", env::current_dir().unwrap().as_path().display().to_string());
+    println!("{}", path_str);
+    let d = DataChartNatal {
+        year: params.year,
+        month: params.month,
+        day: params.day,
+        hour: params.hour,
+        min: params.min,
+        sec: 0.0,
+        lat: params.lat,
+        lng: params.lng,
+    };
+    let dT = DataChartNatal {
+        year: params.yearT,
+        month: params.monthT,
+        day: params.dayT,
+        hour: params.hourT,
+        min: params.minT,
+        sec: 0.0,
+        lat: params.latT,
+        lng: params.lngT,
+    };
+    let res: Vec<DataObjectSvg> = astrology::svg_draw::chart_with_transit(600.0, d, dT, path_str.as_str(), Language::French);
     let mut svg_res: String = "".to_string();
     for r in res.clone() {
         if r.object_type == DataObjectType::Chart {
@@ -361,5 +547,24 @@ pub struct MyNatalParams {
     min: i32,
     lat: f32,
     lng: f32,
+    aspect: i32,
+}
+/// NewForm for js/ts front
+#[derive(Serialize, Deserialize)]
+pub struct MyTransitParams {
+    year: i32,
+    month: i32,
+    day: i32,
+    hour: i32,
+    min: i32,
+    lat: f32,
+    lng: f32,
+    yearT: i32,
+    monthT: i32,
+    dayT: i32,
+    hourT: i32,
+    minT: i32,
+    latT: f32,
+    lngT: f32,
     aspect: i32,
 }
