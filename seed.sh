676b0fd89bb5d52e324131283707d1c0e302ce81
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-21 23:28:44 +0200

    automated

--- seed.sh
+++ seed.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-# Copy before the "migration" directory and file "diesel.toml" from 
-# https://github.com/stephaneworkspace/city_time_zone_sqlite
+rm -rf migrations
+rm diesel.toml
+rm .env
+git clone https://github.com/stephaneworkspace/city_time_zone_sqlite.git
+cd city_time_zone_sqlite
+mv migrations ../migrations
+mv diesel.toml ../diesel.toml
+mv .env ../.env
+cd ..
+rm -rf city_time_zone_sqlite
 DB=$(grep DATABASE_URL .env | cut -d '=' -f 2-)
 rm $DB
 diesel migration run
