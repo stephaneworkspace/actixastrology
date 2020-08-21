91591888685538d5dade7df0c1f35aa378296691
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-21 13:50:50 +0200

    add diesel

--- seed.sh
+++ seed.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Copy before the "migration" directory and file "diesel.toml" from 
+# https://github.com/stephaneworkspace/city_time_zone_sqlite
 DB=$(grep DATABASE_URL .env | cut -d '=' -f 2-)
 rm $DB
 diesel migration run
